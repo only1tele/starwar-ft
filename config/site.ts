63a3131783409ee93dddecb2df840e22ce56d7ef
Tele Falola <oluwateledemi@gmail.com>
2024-09-28 10:44:12 +0100

    initial commit

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Starwar",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+}
